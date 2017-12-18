883ee52ed84f8998271f87736b99d74e14221b89
DenisVolhonsky <dvolhonsky@gmail.com>
2017-12-18 12:24:45 +0200

    interpolation

--- ES6_1/script.js
+++ ES6_1/script.js
@@ -9,29 +9,22 @@ const UPPERLENGTH = UPPER.length;
 const MIDDLELENGTH = MIDDLE.length;
 const LOWLENGTH = LOW.length;
 
-console.log(`Верхний ряд клавиатуры: ${UPPER} содержит: ${UPPERLENGTH} символов`);
-console.log(`Средний ряд клавиатуры: ${MIDDLE} содержит: ${MIDDLELENGTH} символов`);
-console.log(`Нижний ряд клавиатуры: ${LOW} содержитЖ ${LOWLENGTH} символов`);
+console.log(`Верхний ряд клавиатуры: "${UPPER}" содержит: "${UPPERLENGTH}" символов`);
+console.log(`Средний ряд клавиатуры: "${MIDDLE}" содержит: "${MIDDLELENGTH}" символов`);
+console.log(`Нижний ряд клавиатуры: "${LOW}" содержитЖ "${LOWLENGTH}" символов`);
 
 //  3.Используя charAt, для каждой строки получить первый и последний символ для каждой из трех строк,
 //    используйте имеющиеся константы.
-UPPER.charAt(0);
-UPPER.charAt(UPPERLENGTH-1);
 
-MIDDLE.charAt(0);
-MIDDLE.charAt(MIDDLELENGTH-1);
-
-LOW.charAt(0);
-LOW.charAt(LOWLENGTH-1);
+console.log(`Первый символ верхней строки "${UPPER.charAt(0)}", а последний "${UPPER.charAt(UPPERLENGTH-1)}" `)
+console.log(`Первый символ средней строки "${MIDDLE.charAt(0)}", а последний "${MIDDLE.charAt(MIDDLELENGTH-1)}" `)
+console.log(`Первый символ нижней строки "${LOW.charAt(0)}", а последний "${LOW.charAt(LOWLENGTH-1)}" `)
 
 //  4.Сделайте поиск позиции для символов [ и ] с помощью indexOf и сохраните результат в переменных.
-
-
-
-
-
-
-
+let leftBracketPos = UPPER.indexOf('[');
+let rightBracketPos = UPPER.indexOf(']');
+console.log(`Символ "[" по счету ${leftBracketPos}-й`);
+console.log(`Символ "]" по счету ${rightBracketPos}-й`);
 
 //  Используя многострочную шаблонную строку, вывести названия всех переменных и полученные значения
 //  за один console.log.
