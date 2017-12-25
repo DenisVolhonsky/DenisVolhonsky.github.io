293389d439cb0f9bdc2c361c6664c11d083107f3
DenisVolhonsky <dvolhonsky@gmail.com>
2017-12-25 17:48:30 +0200

    added new arr from random chars and length from prompt

--- ES6_6/script.js
+++ ES6_6/script.js
@@ -1,11 +1,17 @@
+const getRandomInt = (min, max) => {   // рандом целых чисел
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+
 let keyTrainer = {
     chars: ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o',
         'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm'],
     charCount: '',
     setCharCount: function () {
         let k=0;
+        let result;
         while(k==0) {
-            let result = prompt(`Введите целое число:`);
+            result = prompt(`Введите целое число:`);
             if(this.checkPositiveInteger(result)) {
                 this.charCount = result;
                 k=1;
@@ -15,16 +21,26 @@ let keyTrainer = {
                k=0;
             }
         }
+        return result;
     },
     checkPositiveInteger: function (response) {  //проверяет является ли число целым положительным числом
         if (response>0 && parseInt( response ) == response) flag = true;
         else flag = false; // меньше нуля и не целое
         return flag;
+    },
+    task: [],
+    createTask: function () {  // создает новый массив длинной из charCount и заполняет его случайными буквами из массива chars
+        lengthNewArr = +this.charCount;
+        for(let i=0; i<lengthNewArr; i++) {
+            randInx = getRandomInt(0, keyTrainer.chars.length);
+            this.task.push(this.chars[randInx]);
+        }
     }
 };
 
 function run() {
     keyTrainer.setCharCount();
+    keyTrainer.createTask();
     console.log(keyTrainer);
 }
 run();
