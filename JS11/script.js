50a12e3af83f4f3b35013bd3e6d8699c0ef6d199
DenisVolhonsky <dvolhonsky@gmail.com>
2017-12-28 01:07:43 +0200

    out content of Local Storage

--- JS11/script.js
+++ JS11/script.js
@@ -1,24 +1,20 @@
 // LocalStorage
 
-
-//     Задание 5. Напишите скрипт, который выводит все содержимое localStorage.
-
-
-//     Задание 1. Напишите программу, которая проверяет поддержку localStorage в браузере.
-//     Результат проверки выведите в консоль.
+//Задание 1. Напишите программу, которая проверяет поддержку localStorage в браузере.
+//Результат проверки выведите в консоль.
 let storageTest = localStorage.setItem('test','localStorage work!!!');
 
 if (localStorage.getItem('test') !== null) {
     storageTest = localStorage.getItem('test');
+    document.getElementById('checkStorage').innerHTML =`${storageTest}`;
     console.log(storageTest);
 }
-else console.log('localStorage error!!!');
-//     Задание 2. Создайте HTML страницу и два варианта оформления, которые размещены
-//     в разных css файлах. На странице сделайте переключатель тем оформления. При выборе
-//     оформления – происходит подключение соответствующего файла CSS. Запишите в localStorage
-//     выбранную тему оформления. При обновлении страницы загружайте ранее выбранную тему.
+else document.getElementById('checkStorage').innerHTML =`localStorage error!!!`;
 
-
+//Задание 2. Создайте HTML страницу и два варианта оформления, которые размещены
+//в разных css файлах. На странице сделайте переключатель тем оформления. При выборе
+//оформления – происходит подключение соответствующего файла CSS. Запишите в localStorage
+//выбранную тему оформления. При обновлении страницы загружайте ранее выбранную тему.
 if (localStorage.getItem('theme') !== null) {
     storageTest = localStorage.getItem('theme');  //загрузить из LS
     document.getElementsByTagName('link')[0].href = storageTest;
@@ -36,8 +32,8 @@ document.getElementById('selectTheme').onclick = function () {
     }
 }
 
-//     Задание 3. Создайте массив, сохраните его в localStorage. Считайте массив из localStorage,
-//     выведите его на экран.
+//Задание 3. Создайте массив, сохраните его в localStorage. Считайте массив из localStorage,
+//выведите его на экран.
 let arr = [19,7,2,7,10,3,14,11,9,22];
 
 localStorage.setItem('array', arr);
@@ -45,13 +41,10 @@ localStorage.setItem('array', arr);
 let storageArr = localStorage.getItem('array');  //загрузить из LS
 document.getElementById('array').innerHTML = `Array: [${storageArr}]`;
 
-//     Задание 4. Создайте окно, которое всплывает через 4 секунды после загрузки страницы.
-
-//     В окне – опция «Согласен». Если пользователь выбирает данную опцию и закрывает окно – то
-
-//     при последующих обновлениях окно не показывается. Если пользователь
-//     не выбрал опцию «Согласен», то повторяйте вывод окна после загрузки.
-
+//Задание 4. Создайте окно, которое всплывает через 4 секунды после загрузки страницы.
+//В окне – опция «Согласен». Если пользователь выбирает данную опцию и закрывает окно – то
+//при последующих обновлениях окно не показывается. Если пользователь
+//не выбрал опцию «Согласен», то повторяйте вывод окна после загрузки.
 let storeflag;
 
 if (localStorage.getItem('flag') !== null) {  //повторный запуск
@@ -72,7 +65,10 @@ let check = (flag) => {
 
 check(storeflag);
 
-
-
-
+//Задание 5. Напишите скрипт, который выводит все содержимое localStorage.
+// document.getElementById('content').innerHTML += `${localStorage[key]}`;
+for(let key in localStorage) {
+    document.getElementById('content').innerHTML += `${key} : ${localStorage[key]}<br>`;
+    console.log(key, localStorage[key]);
+}
 
