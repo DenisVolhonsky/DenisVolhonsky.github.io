16f204d0da256bc659d694f0615c2f8adc2a0137
DenisVolhonsky <dvolhonsky@gmail.com>
2018-01-10 18:30:54 +0200

    create template keyboard

---
+++ ES6_8/script.js
@@ -0,0 +1,28 @@
+/*
+ Напишите скрипт который реализует следующее поведение:
+
+ - При нажатии на клавишу (не виртуальной клавиатуры) должно
+  обрабатываться событие keydown.
+  (Для обработки нажатия нажатия клавиш, повесьте слушателя на window.
+  window.addEventListener("keydown", callback);)
+
+ - Должны обрабатываться только те клавиши, которые присутствуют
+  в разметке HTML (на виртуальной клавиатуре).
+
+ - Звук нажатия на клавишу должен соответсвовать ноте, описанной
+  в атрибуте button data-note.
+
+ - Подсветку текущей клавиши реализуйте с помощью класса
+  keyboard__btn--active.
+
+ - Чекбокс Sound должен включать и выключать звук нажатия на клавиши.
+*/
+
+const playSound = note => {
+    const audio = document.querySelector(`audio[data-note=${note}]`);
+    audio.currentTime = 0;
+    audio.play();
+};
+
+const buttons = Array.from(document.querySelectorAll("button"));
+const keys = "qwertyuiop[]asdfghjkl;'zxcvbnm,./".split("");
