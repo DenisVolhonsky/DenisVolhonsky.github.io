c7591ae7019dc4f8beb905778c12853ddb6e0d9b
DenisVolhonsky <dvolhonsky@gmail.com>
2018-01-26 11:44:06 +0200

    created prototype and constructor

--- ES6_14/script.js
+++ ES6_14/script.js
@@ -4,3 +4,27 @@
 //     При нажатии на кнопку stop, функция сохраняет значение текущего момента времени в stopTime и записывает разницу между startTime и stopTime в interval.
 //     При нажатии на stop, значение interval выводится в консоль.
 
+function Timer() {
+    this.startTime = '';
+    this.stopTime = '';
+    this.interval= ''; // вывод в консоль
+}
+
+
+
+Timer.prototype.start = function () {
+    console.log(1);
+}
+Timer.prototype.stop.stop = function () {
+    console.log(2);
+}
+
+let timer = new Timer();
+
+
+console.log(timer);
+
+
+
+
+
