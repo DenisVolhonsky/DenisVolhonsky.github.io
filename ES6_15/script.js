68a1810345654d69f892afbf484164c0ff19b13c
DenisVolhonsky <dvolhonsky@gmail.com>
2018-01-27 14:37:17 +0200

    all instance of a class is working

--- ES6_15/script.js
+++ ES6_15/script.js
@@ -36,42 +36,46 @@ const stopTime = document.querySelector('#stop-time');
 //     Создать две кнопки в HTML: start и stop.
 //     Создать класс Timer с полями startTime, stopTime и interval.
 //     Создать несколько экземпляров класса с разными значениями свойств, вывести их в консоль.
-
 //     Для класса Timer создать методы start и stop, getTime.
 //     Создать экземпляр класса Timer, пусть он называется stopwatch.
 //     При нажатии на кнопку start, метод stopwatch.start сохраняет момент
 //     нажатия в свойство startTime.
 //     При нажатии на кнопку stop, метод stopwatch.stop сохраняет значение текущего момента
 //     времени в stopTime и записывает разницу между startTime и stopTime в interval.
+
 //     А метод stopwatch.getTime возвращает значение поля interval, которое необходимо вывести в консоль.
 //     Для класса Timer создать статический метод timeToNY который возвращает кол-во дней от сегодня и до Нового Года.
 
 
 class Timer {
     constructor() {
-        this.startTime = 1;
-        this.stopTime = 2;
-        this.interval = 3; // вывод в консоль
+        this.startTime = '';
+        this.stopTime = '';
+        this.interval = ''; // вывод в консоль
     }
     start() {
-        console.log('start');
+        stopWatch.startTime = new Date();
+        firstTime.innerHTML=`${stopWatch.startTime.getHours()}:${stopWatch.startTime.getMinutes()}:${stopWatch.startTime.getSeconds()}`;
+        console.log(stopWatch.startTime);
     }
     stop() {
-        console.log('stop');
+        stopWatch.stopTime = new Date();
+        console.log(stopWatch.stopTime);
+        stopTime.innerHTML=`${stopWatch.stopTime.getHours()}:${stopWatch.stopTime.getMinutes()}:${stopWatch.stopTime.getSeconds()}`;
+        stopWatch.interval=((stopWatch.stopTime-stopWatch.startTime)/1000).toFixed(1);
+        console.log(stopWatch.interval);
     }
     getTime(){
-        console.log('getTime');
+
     }
 
 }
 
-timer1= new Timer();
-timer2= new Timer();
-timer3= new Timer();
+stopWatch = new Timer(); // экземпляр класса
 
-console.log(timer1.startTime, timer2.stopTime, timer3.interval);
+//console.log(stopWatch.startTime, stopWatch.stopTime, stopWatch.interval);
 
 
-startBtn.addEventListener('click', timer1.start);
-stopBtn.addEventListener('click', timer1.stop);
+startBtn.addEventListener('click', stopWatch.start);
+stopBtn.addEventListener('click', stopWatch.stop);
 
