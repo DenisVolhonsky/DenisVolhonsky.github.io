72f421f22599ca516678f4034a12a59ffbaa5777
DenisVolhonsky <dvolhonsky@gmail.com>
2018-01-30 16:16:11 +0200

    created new methods

--- ES6_16/script.js
+++ ES6_16/script.js
@@ -23,46 +23,81 @@
 //     draw() - метод который имитирует рисование круга используя методы Shape и Circle,
 //     выводя в консоль браузера следующей информации.
 
+//////////////////Globals//////////////////////
+let shapeColor = document.querySelector('#shape-color');
+let shapeX = document.querySelector('#shape-x');
+let shapeY = document.querySelector('#shape-y');
+let shapeSave = document.querySelector('#shape-save');
+let getSave = document.querySelector('#get-save');
+
+let rectWidth = document.querySelector('#width');
+let rectHeight = document.querySelector('#height');
+let rectSave = document.querySelector('#rect-save');
+let rectOut = document.querySelector('#rect-out');
+
+let circRadius = document.querySelector('#radius');
+let circleSave = document.querySelector('#circle-save');
+let outCircle = document.querySelector('#circle-out');
+
 class Shape {
     constructor(color, initX, initY){
         this.color = 'red';
-        this.initX = '12';
-        this.initY = '10';
+        this.initX = 20;
+        this.initY = 20;
     }
     getColor() {
-        console.log(`Цвет: ${this.color}`);
+        console.log(`Цвет1: ${this.color}`);
     }
     setColor(val) {
-
+        this.color = val;
+        console.log(`Данные экземпляра изменены!!!`);
     }
     getCoords() {
         console.log(`Координата X: ${this.initX}, Координата Y: ${this.initY}`);
     }
     moveTo(newX, newY) {
-
+        this.initX = newX;
+        this.initY = newY;
     }
-
 }
 
+let shape = new Shape();
+console.log(`Св-ва экземпляра по умолчанию:`);
+console.log(shape);
+
+shapeSave.addEventListener('click', ()=> [shape.setColor(shapeColor.value),shape.moveTo(shapeX.value, shapeY.value)]);
+getSave.addEventListener('click', ()=>[shape.getColor(),shape.getCoords(),console.log(shape)]);
+
 class Rectangle extends Shape {
-    constructor(params, initWidth, initHeight){
-        super(params);
-        this.initWidth = initWidth;
-        this.initHeight = initHeight;
+    constructor(){
+        super();
+        this.initWidth = 100;
+        this.initHeight = 200;
     }
     setWidth(newWidth){
-
+        this.initWidth = newWidth;
     }
     setHeight(newHeight) {
-
+        this.initHeight = newHeight;
     }
     getDims(){
         console.log(`Высота прямоугольника: ${this.initWidth}, ширина: ${this.initHeight}`);
     }
-    draw() {   // смотри скриншот
-        console.log();
+    draw() {
+        rectOut.innerText =
+        `Drawing a Rectangle at:
+        (x: ${this.initX}, y: ${this.initY})
+        With dimentions:
+        width: ${this.initWidth}
+        height: ${this.initHeight}
+        Filled with color: ${this.color}
+        `;
     }
 }
+
+let rectangle = new Rectangle();
+rectSave.addEventListener('click', ()=>[rectangle.setWidth(rectWidth.value),rectangle.setHeight(rectHeight.value),rectangle.getDims(),rectangle.draw()]);
+
 
 class Circle extends Shape {
     constructor(params, initRadius){
@@ -75,14 +110,17 @@ class Circle extends Shape {
     setRadius(val){
 
     }
-    draw(){    // смотри скриншот
+    draw(){
 
     }
 }
 
-let rectangle = new Rectangle();
+
+
 let circle = new Circle();
 
-console.log(rectangle);
-console.log(circle);
+
+
+
+
 
